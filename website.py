cb68921b2c5141aade3c84e55aff4fb335351d12
Hattinger04 <s.8greiderer@gmail.com>
2021-12-28 16:31:36 +0100

    sending data between template and python file

--- website.py
+++ website.py
@@ -3,16 +3,25 @@ from flask.templating import render_template
 from model import Question, Module
 
 app = Flask(__name__)
-
+app.secret_key = '5#y2L"F4Q8zsa7Zb'
 
 @app.route('/')
 def startseite():
     return render_template('startseite.html', result = dict)
 
-@app.route('/game')
-@app.route('/game/<int:answer>')
+@app.route('/game', methods = ['GET', 'POST'])
+@app.route('/game/<int:answer>', methods = ['GET', 'POST'])
 def game(answer=-1):
-    return render_template('game.html', result = dict)
+    data = [
+        {
+            'frage': 'fragen über fragen...',
+            'antwort1': 'antowort1...',
+            'antwort2': 'antowort2...',
+            'antwort3': 'antowort3...',
+            'antwort4': 'antowort4...'
+        }
+    ]
+    return render_template('game.html', data=data, result = dict)
 
 @app.route('/questions')
 def questions():
