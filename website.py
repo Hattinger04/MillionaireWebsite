8fc08f90e079f94dd8221bc1c368e69f1006ed7b
Hattinger04 <s.8greiderer@gmail.com>
2021-12-31 15:08:15 +0100

    showing all questions in site with filter option

--- website.py
+++ website.py
@@ -6,6 +6,24 @@ app = Flask(__name__)
 app.secret_key = '5#y2L"F4Q8zsa7Zb'
 questions = Module.read_questions("millionaire.txt")
 player_level = -1
+right_answer = -1
+
+def new_question(level): 
+    global right_answer
+    question = Module.get_rand_question(level, questions)
+    right_answer = question.antwort
+    data = [
+        {
+            'frage': question.fragetext,
+            'level': question.level,
+            'antwort1': question.antwortmoeglichkeit[0],
+            'antwort2': question.antwortmoeglichkeit[1],
+            'antwort3': question.antwortmoeglichkeit[2],
+            'antwort4': question.antwortmoeglichkeit[3]
+        }
+    ]
+    return data
+
 
 @app.route('/')
 def startSite():
@@ -16,35 +34,27 @@ def startSite():
 def gameSite(answer=-1):
     global player_level
     player_level += 1
-    if(player_level != 0): 
-        pass
-        # get value and check if correct
-
-        # if correct next question
-
-        # else return false
-
+    if(player_level == 5): 
+        # return winner site
+        return render_template('game.html', data=new_question(player_level), result = dict)
+    elif(player_level != 0): 
+        if answer != right_answer:
+            # return to smth different or smth like that when not right answer
+            return render_template('game.html', data=new_question(player_level), result = dict)
+        return render_template('game.html', data=new_question(player_level), result = dict)
     else: 
-        # first Question: 
-        question = Module.get_rand_question(0, questions)
-
-        data = [
-            {
-                'frage': question.fragetext,
-                'level': question.level,
-                'antwort1': question.antwortmoeglichkeit[0],
-                'antwort2': question.antwortmoeglichkeit[1],
-                'antwort3': question.antwortmoeglichkeit[2],
-                'antwort4': question.antwortmoeglichkeit[3]
-            }
-           
-    ]
-    return render_template('game.html', data=data, result = dict)
-    
+       return render_template('game.html', data=new_question(player_level), result = dict)    
 
 @app.route('/questions')
 def questionsSite():
-    return render_template('questions.html', result = dict) 
+    fragen = []
+    for q in questions: 
+        fragen.append({
+            "level": q.level, 
+            "frage": q.fragetext
+        })
+    return render_template('questions.html', data=fragen, result = dict) 
+
 
 
 if __name__ == '__main__':
